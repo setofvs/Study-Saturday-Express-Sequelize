a768cbb03e3f75433825a394c8de9c22b13e98fd
Lawrence Wang <setofvs@gmail.com>
2019-04-27 17:40:26 -0400

    Cycle-4

--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,59 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Test = require("../db/models/test");
+const Student = require("../db/models/student");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const tests = await Test.findAll();
+    res.send(tests);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const testId = req.params.id;
+    const testByPk = await Test.findByPk(testId);
+    res.send(testByPk);
+  } catch (err) {
+    next(err);
+  }
+});
+//sequelize will make associations between two tables for you when you use the .belongsTo, .hasMany, etc.
+router.post("/student/:studentId", async (req, res, next) => {
+  try {
+    // METHOD 1:
+    const student = await Student.findById(req.params.studentId);
+    const test = await Test.create(req.body);
+    const studentTest = await test.setStudent(student);
+
+    //METHOD 2:
+    // const student = await Student.findById(req.params.studentId);
+    // const test = await Test.create(req.body);
+    // await test.update({ studentId: Number(req.params.studentId) });
+
+    // METHOD 3:
+    // const { subject, grade } = req.body;
+    // const studentPk = req.params.studentId;
+    // const studentTest = await Test.create({ subject, grade, studentPk });
+    res.status(201).send(studentTest);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Test.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
